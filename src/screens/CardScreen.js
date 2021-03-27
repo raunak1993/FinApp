67700fad18035bdf41bc99f2228e1727ab1c6bcd
Raunak Tibrewal <rtibrewal@gammastack.com>
2021-03-27 15:08:21 +0530

    card screen ui design

--- src/screens/CardScreen.js
+++ src/screens/CardScreen.js
@@ -1,15 +1,58 @@
 import React from 'react';
 
-import { View, Text } from "react-native";
+import { View, Text, StyleSheet, Image } from "react-native";
 
-const CardScreen = () => {
+import CARD_IMG from "../assets/card.png";
+
+const CardScreen = ({ navigation }) => {
     return (
-        <View>
-            <Text>
-                Create Virtual Card
+        <View style={styles.container}>
+            <View style={styles.heading_box}>
+                <Text style={styles.heading}>
+                    Create Virtual Card
+                </Text>
+                <Text style={styles.caption}>
+                    Fresh Virtual credit card are on your way!
             </Text>
+            </View>
+            <View style={styles.card_box}>
+                <View style={styles.card_image_box}>
+                    <Image style={styles.card_image} source={CARD_IMG} />
+                </View>
+                <View style={styles.card_detail_box}>
+
+                </View>
+            </View>
+
         </View>
     );
 };
+
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        paddingHorizontal: 20
+    },
+    heading_box: {
+        flexBasis: '15%',
+        justifyContent: 'center'
+    },
+    heading: {
+        fontSize: 18,
+        fontWeight: 'bold'
+    },
+    caption: {
+        marginTop: 10,
+        color: 'darkgrey'
+    },
+    card_box: {
+        flexBasis: '75%',
+        borderColor: 'red',
+        borderWidth: 1,
+        position: 'relative'
+    }
+
+});
 
 export default CardScreen;
